cc7a8afd6e96824810c259a51787507a78164474
drissakande <drissakande@gmail.com>
2023-05-22 11:18:41 -0400

    Add code in index.js

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+ 
+    //Create a set to store the complement of each element
+    const seenNumbers = new Set();
+
+  for (let num of array) {
+    const complement = target - num;
+
+    // Check if the current element's complement exists in the set
+    if (seenNumbers.has(complement)) {
+      return true;
+    }
+
+    seenNumbers.add(num);
+  }
+
+  return false;
 }
+
+
+  
+  
+
 
 /* 
   Write the Big O time complexity of your function here
 */
+//time complexity of the hasTargetSum function is O(n), where n is the length of the array.
 
 /* 
   Add your pseudocode here
@@ -13,6 +35,8 @@ function hasTargetSum(array, target) {
 /*
   Add written explanation of your solution here
 */
+// we iterate over each element of the array using a for...of loop. For each element, we calculate its complement. 
+//We use a Set called seenNumbers to store the numbers we have seen so far.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -29,6 +53,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  console.log("=>", hasTargetSum([7, 9, 2, 2, 5], 20));
+  console.log("=>", hasTargetSum([5,10, 6, 2, 5], 7));
+  console.log("=>", hasTargetSum([1,3, 0, 15, 11, 25, 2, 5], 15));
 }
 
 module.exports = hasTargetSum;
